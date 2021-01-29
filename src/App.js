33bc9eb1d85b293659d995a269c20796b181e061
James Baldwin <jamescbaldwin@gmail.com>
2021-01-29 15:17:08 -0500

    components determined

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Search from "./components/pages/Home";
-import About from "./components/pages/About";
-import Discover from "./components/pages/Blog";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper"
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavTabs />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/blog" component={Blog} />
-        <Route path="/contact" component={Contact} />
+      <div className="App">
+        <Wrapper>
+          <Header />
+          <Main />
+        </Wrapper>
       </div>
-    </Router>
   );
 }
 
---
+++ src/components/Body.js
@@ -0,0 +1,2 @@
+import React from "react";
+//table of data with sortable columns
